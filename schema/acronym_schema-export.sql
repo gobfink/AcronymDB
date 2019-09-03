2a3c9619865902f2dba69ca663650ae067fa53eb
Paviliong7 <paviliong7@pavilion.andy>
2019-09-03 13:50:04 -0400

    updated Schema for Acro Database

--- schema/acronym_schema-export.sql
+++ schema/acronym_schema-export.sql
@@ -5,14 +5,10 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP DATABASE IF EXISTS `acronym`;
-CREATE DATABASE `acronym` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `acronym`;
-
 DROP TABLE IF EXISTS `tbl_AcroTag`;
 CREATE TABLE `tbl_AcroTag` (
-  `acroID` int(11) NOT NULL,
-  `tagID` int(11) NOT NULL,
+  `acroID` bigint(20) NOT NULL,
+  `tagID` bigint(20) NOT NULL,
   PRIMARY KEY (`acroID`,`tagID`),
   KEY `acroID` (`acroID`) USING BTREE,
   KEY `tagID` (`tagID`) USING BTREE
@@ -21,19 +17,26 @@ CREATE TABLE `tbl_AcroTag` (
 
 DROP TABLE IF EXISTS `tbl_Acronym`;
 CREATE TABLE `tbl_Acronym` (
-  `acroID` int(11) NOT NULL AUTO_INCREMENT,
+  `acroID` bigint(20) NOT NULL AUTO_INCREMENT,
   `acronym` varchar(80) NOT NULL,
   `definition` varchar(80) NOT NULL,
-  `authID` int(11) DEFAULT NULL,
+  `authID` bigint(20) DEFAULT NULL,
   `dateCreate` datetime DEFAULT NULL,
   PRIMARY KEY (`acroID`),
   KEY `acroID` (`acroID`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
-DROP TABLE IF EXISTS `tbl_user`;
-CREATE TABLE `tbl_user` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `tbl_Tag`;
+CREATE TABLE `tbl_Tag` (
+  `TagID` bigint(20) NOT NULL,
+  `Tag` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `tbl_User`;
+CREATE TABLE `tbl_User` (
+  `userID` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(80) NOT NULL,
   `userFN` varchar(80) DEFAULT NULL,
   `userLN` varchar(80) DEFAULT NULL,
