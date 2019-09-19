92483d960fd0774e6308d4dcda1e397f202d2e52
Kurt Gustafson <kgustafson2@gmail.com>
2019-09-19 09:06:23 -0400

    modified templates and base code for listing Acronyms

--- flask/code/app/home/views.py
+++ flask/code/app/home/views.py
@@ -2,6 +2,7 @@
 
 from flask import abort, render_template
 from flask_login import current_user, login_required
+from ..models import Acronym
 
 from . import home
 
@@ -21,6 +22,17 @@ def dashboard():
     """
  
     return render_template('home/dashboard.html',title="Dashboard")
+
+@home.route('/acronyms')
+@login_required
+def acronyms():
+    """
+    List all acronyms
+    """
+  
+    acronyms = Acronym.query.all()
+    return render_template('home/acronyms/acronyms.html',
+                           acronyms=acronyms, title="Acronyms") 
 
 @home.route('/admin/dashboard')
 @login_required
