7d6aef7c592ec7d34decaa3478af14eb5239749b
Kurt Gustafson <kurt.gustafson@assett.net>
2019-10-01 17:22:02 -0400

    Modify for User edit.  Not complete

--- flask/code/app/admin/forms.py
+++ flask/code/app/admin/forms.py
@@ -1,15 +1,27 @@
 # app/admin/forms.py
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
-from ..models import Tag
+from ..models import Tag, User
 
 class TagsForm(FlaskForm):
     """
     Form for admin to add or edit tags
     """
     tag = StringField('Tag', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class UsersForm(FlaskForm):
+    """
+    Form for admin to edit users
+    """
+    
+    username = StringField('Username', validators=[DataRequired()])
+    userEmail = StringField('Email', validators=[DataRequired()])
+    userFN = StringField('First Name', validators=[])
+    userLN = StringField('Last Name', validators=[])
+    userIsAdmin = SelectField('Is Admin?', choices=[(1,'Yes'),(0,'No')])
     submit = SubmitField('Submit')
--- flask/code/app/admin/views.py
+++ flask/code/app/admin/views.py
@@ -5,9 +5,9 @@ from flask_login import current_user, login_required
 
 from . import admin
 from .. import db
-from ..models import Tag
+from ..models import Tag, User
 
-from . forms import TagsForm
+from . forms import TagsForm, UsersForm
 
 
 def check_admin():
@@ -114,3 +114,94 @@ def delete_tag(id):
     return redirect(url_for('admin.list_tags'))
 
     return render_template(title="Delete Tag")
+
+
+@admin.route('/users', methods=['GET', 'POST'])
+@login_required
+def list_users():
+    """
+    List all users
+    """
+    check_admin()
+
+    users = User.query.all()
+
+    return render_template('admin/users/users.html',
+                           users=users, title="Users")
+
+@admin.route('/users/add', methods=['GET', 'POST'])
+@login_required
+def add_user():
+    """
+    Add a user to the database
+    """
+    check_admin()
+
+    add_user = True
+
+    form = UsersForm()
+
+    if form.validate_on_submit():
+        user = User(userEmail=form.userEmail.data,
+                            username=form.username.data,
+                            userFN=form.userFN.data,
+                            userLN=form.userLN.data,
+                            password=form.password.data,
+                            userLastLoginDT=datetime.datetime.now(),
+                            userIsAdmin=form.userIsAdmin.data)
+        # add user to the database
+        db.session.add(user)
+        db.session.commit()
+        flash('You have successfully registered! You may now login.')
+
+        # redirect to the users page
+        return redirect(url_for('auth.list_users'))
+  
+    #load new user template
+    return render_template('admin/users/user.html', action="Add",
+                           add_user=add_user, form=form,
+                           title="Add User")
+
+@admin.route('/users/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit_user(id):
+    """
+    Edit a user
+    """
+    check_admin()
+
+    add_user = False
+
+    user = User.query.get_or_404(id)
+    form = UsersForm(obj=user)
+    if form.validate_on_submit():
+	# handle the edit
+        return redirect(url_for('admin.list_users'))
+
+    form.username.data = user.username
+    form.userEmail.data = user.userEmail
+    form.userFN.data = user.userFN
+    form.userLN.data = user.userLN
+    form.userIsAdmin.data = user.userIsAdmin
+    return render_template('admin/users/user.html', action="Edit",
+                           add_user=add_user, form=form,
+                           user=user, title="Edit User")
+
+@admin.route('/users/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_user(id):
+    """
+    Delete a user from the database
+    """
+    check_admin()
+
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('You have successfully deleted the user.')
+
+    # redirect to the tags page
+    return redirect(url_for('admin.list_users'))
+
+    return render_template(title="Delete User")
+
