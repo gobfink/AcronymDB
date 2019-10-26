e4eb5d7f1e1a9773d11d153a314343d760573135
mint <andy.fortman@assett.net>
2019-10-26 00:15:37 -0400

    removed old docker-compose directory

--- docker-compose/flask/code/dbconn.py
+++
@@ -1,9 +0,0 @@
-import MySQLdb
-
-def connection():
-    conn = MySQLdb.connect(host="db",
-                           user="root",
-                           passwd="example",
-                           db="acronym")
-    c = conn.cursor()
-    return c, conn
